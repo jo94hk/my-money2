92c3466a183fef94c2fb907dfccf1258a2d33903
Moon Young Doon <jo94hk@gamil.com>
2024-06-19 14:27:50 +0900

    네브바 컴포넌트

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Signup from "./pages/signup/Signup";
+import Navbar from "./Components/Navbar";
 
 function App() {
   return (
-   <div>
-    앱시작
-   </div>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,15 @@
+import { Link } from 'react-router-dom';
+import styles from './Navbar.module.css';
+
+export default function Navbar() {
+  return (
+    <nav className={styles.navbar}>
+      <ul>
+        <li className={styles.title}><Link to="/">myMoney</Link></li>
+
+        <li><Link to="/login">로그인</Link></li>
+        <li><Link to="/signup">가입</Link></li>
+      </ul>
+    </nav>
+  );
+}
