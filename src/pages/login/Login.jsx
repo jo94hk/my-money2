24b16932e9596324e58900f5f70059a00b903511
Moon Young Doon <jo94hk@gamil.com>
2024-06-19 14:53:27 +0900

    로그인페이지 정리

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,9 +1,38 @@
-import styles from './Login.module.css';
+import { useState } from "react";
+import styles from "./Login.module.css";
 
 function Login() {
+  const [email, setEmail] = useState("");
+  const [passowrd, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(email, passowrd);
+  };
   return (
-    <div>Login</div>
-  )
+    <form onSubmit={handleSubmit} className={styles["login-form"]}>
+      <h2>Login</h2>
+      <label>
+        <span>email:</span>
+        <input
+          type="email"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+      </label>
+      {/* 패스워드 라벨과 입력창도 완성한다! */}
+      <label>
+        <span>passowrd:</span>
+        <input
+          type="passowrd"
+          onChange={(e) => setPassword(e.target.value)}
+          value={passowrd}
+        />
+      </label>
+
+      <button className="btn">로그인</button>
+    </form>
+  );
 }
 
-export default Login
+export default Login;
